8b10e9cdd72907d7f128f714a59ec775591d77db
ecosistema-rodri <economicassomos@gmail.com>
2026-08-19 22:11:25 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // ÚNICA configuración pública del frontend.
 // Reemplazá esta URL una sola vez después de crear el Web Service en Render.
 window.MAPA_CONFIG = {
-  apiBase: 'https://TU-SERVICIO.onrender.com'
+  apiBase: 'https://flash-work-mapa-editor.onrender.com'
 };
